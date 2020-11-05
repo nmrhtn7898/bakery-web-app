cb0fdbde9b3f623ae6dc008226e8e9a0386cddb1
SYJ AT HOME <nmrhtn7898@naver.com>
2020-11-05 21:08:54 +0900

    빌드 스크립트 수정

---
+++ bread-api-module/src/test/java/TestApplication.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestApplication {
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+}
