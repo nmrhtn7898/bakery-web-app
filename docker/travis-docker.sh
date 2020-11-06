c60d7b67e154b7e40b4b394b0bcd9be80f068295
sin <nmrhtn7898@naver.com>
2020-11-06 13:58:17 +0900

    travis 빌드 테스트 15

--- docker/travis-docker.sh
+++ docker/travis-docker.sh
@@ -1,5 +1,5 @@
-./gradlew :$MODULE_NAME:clean build createDockerImage
-docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+./gradlew :$MODULE_NAME:clean :$MODULE_NAME:build :$MODULE_NAME:createDockerImage
+echo $DOCKER_PASSWORD | docker login --username $DOCKER_USERNAME --password-stdin
 docker tag $DOCKER_USERNAME/$MODULE_NAME:$MODULE_VERSION $DOCKER_USERNAME/MODULE_NAME
 docker push $DOCKER_USERNAME/MODULE_NAME:$MODULE_VERSION
 docker push $DOCKER_USERNAME/MODULE_NAME
