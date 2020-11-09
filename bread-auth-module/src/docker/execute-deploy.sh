74b6226195cc41654d66f96090f7af622ae68dc9
sin <nmrhtn7898@naver.com>
2020-11-09 16:45:56 +0900

    빌드 배포 자동화 테스트 12

--- bread-auth-module/src/docker/execute-deploy.sh
+++ bread-auth-module/src/docker/execute-deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo $HOME/deploy/deploy-sh/bread-auth-module-deploy.sh > /dev/null 2> /dev/null < /dev/null &
+$HOME/deploy/deploy-sh/bread-auth-module-deploy.sh > /dev/null 2> /dev/null < /dev/null &
