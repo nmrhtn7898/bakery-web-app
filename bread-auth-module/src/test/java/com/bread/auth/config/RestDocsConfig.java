74cc45411e80590a0087be049a8c768be7a4f08d
sin <nmrhtn7898@naver.com>
2020-11-11 14:20:59 +0900

    restdocs asciidoctor 커스텀 설정 적용

--- bread-auth-module/src/test/java/com/bread/auth/config/RestDocsConfig.java
+++ bread-auth-module/src/test/java/com/bread/auth/config/RestDocsConfig.java
@@ -12,10 +12,14 @@ public class RestDocsConfig {
 
     @Bean
     public RestDocsMockMvcConfigurationCustomizer restDocsMockMvcConfigurationCustomizer() {
+        CustomUriModifyingOperationPreprocessor uriProcessor = new CustomUriModifyingOperationPreprocessor();
+        uriProcessor.setBasePath("/auth"); // /api/v1/example -> /auth/api/v1/example 변환
+        uriProcessor.setPort("-1"); // 커스텀 processor 에서 -1 포트는 노출 안함
+        uriProcessor.setHost("ec2-54-180-10-196.ap-northeast-2.compute.amazonaws.com");
         return config -> config
                 .operationPreprocessors()
-                .withRequestDefaults(prettyPrint())
-                .withResponseDefaults(prettyPrint());
+                .withRequestDefaults(prettyPrint(), uriProcessor)
+                .withResponseDefaults(prettyPrint(), uriProcessor);
     }
 
     @Bean
