f741747908898efdd8ffc5927d28ab55fe8c8cbf
sin <nmrhtn7898@naver.com>
2020-12-01 22:18:56 +0900

    AWS elasticache에서 Spring Session 사용하는 경우 발생하는 이슈 해결

--- bread-auth-module/src/main/java/com/bread/auth/config/RedisConfig.java
+++ bread-auth-module/src/main/java/com/bread/auth/config/RedisConfig.java
@@ -10,13 +10,23 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.GenericToStringSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+import static org.springframework.session.data.redis.config.ConfigureRedisAction.NO_OP;
 
 @Profile("!test")
 @EnableRedisRepositories
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800, redisNamespace = "login")
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public ConfigureRedisAction configureRedisAction() {
+        // AWS elasticCache 에서는 사용할 수 있는 notify-keyspace-events 값은 K,E,g,$,l,s,h,z,x,e,A 그 외는 에러를 발생함.
+        // Spring Session 에서 'config' 값으로 notify-keyspace-events 사용하여 에러 발생, 해당 이슈 방지를 위한 설정
+        return NO_OP;
+    }
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory(@Value("${spring.redis.host}") String host,
